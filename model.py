41982c208372e5956f1e10e77918e192a93b4009
geyutang <939718154@qq.com>
2019-03-14 09:32:01 +0800

    modify for tuning

--- model.py
+++ model.py
@@ -59,6 +59,42 @@ class ClassBlock(nn.Module):
         else:
             x = self.classifier(x)
             return x
+class AddBlock(nn.Module):
+    def __init__(self, input_dim, droprate, 
+        relu=False, bnorm=True, embedding=512, linear=True, return_f = False):
+        super(AddBlock, self).__init__()
+        self.return_f = return_f
+        add_block = []
+        if linear:
+            add_block += [nn.Linear(input_dim, embedding)]
+        else:
+            embedding = input_dim
+        if bnorm:
+            add_block += [nn.BatchNorm1d(embedding)]
+        if relu:
+            add_block += [nn.LeakyReLU(0.1)]
+        if droprate>0:
+            add_block += [nn.Dropout(p=droprate)]
+        add_block = nn.Sequential(*add_block)
+        add_block.apply(weights_init_kaiming)
+
+        # classifier = []
+        # classifier += [nn.Linear(num_bottleneck, class_num)]
+        # classifier = nn.Sequential(*classifier)
+        # classifier.apply(weights_init_classifier)
+
+        self.add_block = add_block
+        # self.classifier = classifier
+    def forward(self, x):
+        x = self.add_block(x)
+        return x
+        # if self.return_f:
+        #     f = x
+        #     x = self.classifier(x)
+        #     return x,f
+        # else:
+        #     x = self.classifier(x)
+        #     return x
 
 # Define the ResNet50-based Model
 class ft_net(nn.Module):
@@ -88,11 +124,38 @@ class ft_net(nn.Module):
         x = self.classifier(x)
         return x
 
+class resnet_arc(nn.Module):
+
+    def __init__(self, class_num, droprate=0.5, stride=1):
+        super(resnet_arc, self).__init__()
+        model_ft = models.resnet50(pretrained=True)
+        # avg pooling to global pooling
+        if stride == 1:
+            model_ft.layer4[0].downsample[0].stride = (1,1)
+            model_ft.layer4[0].conv2.stride = (1,1)
+        model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.model = model_ft
+        self.addblock = AddBlock(2048, class_num, droprate)
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.model.avgpool(x)
+        x = x.view(x.size(0), x.size(1))
+        x = self.addblock(x)
+        return x
+
 # Define the DenseNet121-based Model
 class ft_net_dense(nn.Module):
 
     def __init__(self, class_num, droprate=0.5):
-        super().__init__()
+        super(ft_net_dense, self).__init__()
         model_ft = models.densenet121(pretrained=True)
         model_ft.features.avgpool = nn.AdaptiveAvgPool2d((1,1))
         model_ft.fc = nn.Sequential()
@@ -105,7 +168,28 @@ class ft_net_dense(nn.Module):
         x = x.view(x.size(0), x.size(1))
         x = self.classifier(x)
         return x
-    
+
+class dense_arc(nn.Module):
+
+    def __init__(self, embedding, droprate=0.5):
+        super().__init__()
+        model_ft = models.densenet121(pretrained=True)
+        model_ft.features.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        model_ft.fc = nn.Sequential()
+        self.model = model_ft
+        # For DenseNet, the feature dim is 1024 
+        # self.classifier = ClassBlock(1024, class_num, droprate)
+        ##########################################################
+        # add fully connect network
+        self.addblock = AddBlock(1024, droprate, embedding=embedding)
+    def forward(self, x):
+        x = self.model.features(x)
+        x = x.view(x.size(0), x.size(1))
+        ###########################################
+        # add bolock
+        x = self.addblock(x)
+        # x = self.classifier(x)
+        return x
 # Define the ResNet50-based Model (Middle-Concat)
 # In the spirit of "The Devil is in the Middle: Exploiting Mid-level Representations for Cross-Domain Instance Matching." Yu, Qian, et al. arXiv:1711.08106 (2017).
 class ft_net_middle(nn.Module):
--- test.py
+++ test.py
@@ -16,230 +16,260 @@ import time
 import os
 import scipy.io
 import yaml
-from model import ft_net, ft_net_dense, PCB, PCB_test
+from model import ft_net, ft_net_dense, PCB, PCB_test, resnet_arc, dense_arc
 
-#fp16
-try:
-    from apex.fp16_utils import *
-except ImportError: # will be 3.x series
-    print('This is not an error. If you want to use low precision, i.e., fp16, please install the apex with cuda support (https://github.com/NVIDIA/apex) and update pytorch to 1.0')
-######################################################################
+# #fp16
+# try:
+#     from apex.fp16_utils import *
+# except ImportError: # will be 3.x series
+#     print('This is not an error. If you want to use low precision, i.e., fp16, please install the apex with cuda support (https://github.com/NVIDIA/apex) and update pytorch to 1.0')
+# ######################################################################
 # Options
 # --------
-
-parser = argparse.ArgumentParser(description='Training')
-parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
-parser.add_argument('--which_epoch',default='last', type=str, help='0,1,2,3...or last')
-parser.add_argument('--test_dir',default='../Market/pytorch',type=str, help='./test_data')
-parser.add_argument('--name', default='ft_ResNet50', type=str, help='save model path')
-parser.add_argument('--batchsize', default=256, type=int, help='batchsize')
-parser.add_argument('--use_dense', action='store_true', help='use densenet121' )
-parser.add_argument('--PCB', action='store_true', help='use PCB' )
-parser.add_argument('--multi', action='store_true', help='use multiple query' )
-parser.add_argument('--fp16', action='store_true', help='use fp16.' )
-
-opt = parser.parse_args()
-###load config###
-# load the training config
-config_path = os.path.join('./model',opt.name,'opts.yaml')
-with open(config_path, 'r') as stream:
-        config = yaml.load(stream)
-opt.fp16 = config['fp16'] 
-opt.PCB = config['PCB']
-opt.use_dense = config['use_dense']
-
-if 'nclasses' in config: # tp compatible with old config files
-    opt.nclasses = config['nclasses']
-else: 
-    opt.nclasses = 751 
-
-str_ids = opt.gpu_ids.split(',')
-#which_epoch = opt.which_epoch
-name = opt.name
-test_dir = opt.test_dir
-
-gpu_ids = []
-for str_id in str_ids:
-    id = int(str_id)
-    if id >=0:
-        gpu_ids.append(id)
-
-# set gpu ids
-if len(gpu_ids)>0:
-    torch.cuda.set_device(gpu_ids[0])
-    cudnn.benchmark = True
-
-######################################################################
-# Load Data
-# ---------
-#
-# We will use torchvision and torch.utils.data packages for loading the
-# data.
-#
-data_transforms = transforms.Compose([
-        transforms.Resize((256,128), interpolation=3),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-############### Ten Crop        
-        #transforms.TenCrop(224),
-        #transforms.Lambda(lambda crops: torch.stack(
-         #   [transforms.ToTensor()(crop) 
-          #      for crop in crops]
-           # )),
-        #transforms.Lambda(lambda crops: torch.stack(
-         #   [transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(crop)
-          #       for crop in crops]
-          # ))
-])
-
-if opt.PCB:
-    data_transforms = transforms.Compose([
-        transforms.Resize((384,192), interpolation=3),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
-    ])
-
-
-data_dir = test_dir
-
-if opt.multi:
-    image_datasets = {x: datasets.ImageFolder( os.path.join(data_dir,x) ,data_transforms) for x in ['gallery','query','multi-query']}
-    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
-                                             shuffle=False, num_workers=16) for x in ['gallery','query','multi-query']}
-else:
-    image_datasets = {x: datasets.ImageFolder( os.path.join(data_dir,x) ,data_transforms) for x in ['gallery','query']}
-    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
-                                             shuffle=False, num_workers=16) for x in ['gallery','query']}
-class_names = image_datasets['query'].classes
-use_gpu = torch.cuda.is_available()
-
-######################################################################
-# Load model
-#---------------------------
-def load_network(network):
-    save_path = os.path.join('./model',name,'net_%s.pth'%opt.which_epoch)
-    network.load_state_dict(torch.load(save_path))
-    return network
-
-
-######################################################################
-# Extract feature
-# ----------------------
-#
-# Extract feature from  a trained model.
-#
-def fliplr(img):
-    '''flip horizontal'''
-    inv_idx = torch.arange(img.size(3)-1,-1,-1).long()  # N x C x H x W
-    img_flip = img.index_select(3,inv_idx)
-    return img_flip
-
-def extract_feature(model,dataloaders):
-    features = torch.FloatTensor()
-    count = 0
-    for data in dataloaders:
-        img, label = data
-        n, c, h, w = img.size()
-        count += n
-        print(count)
-        ff = torch.FloatTensor(n,512).zero_()
-
-        if opt.PCB:
-            ff = torch.FloatTensor(n,2048,6).zero_() # we have six parts
-        for i in range(2):
-            if(i==1):
-                img = fliplr(img)
-            input_img = Variable(img.cuda())
-            #if opt.fp16:
-            #    input_img = input_img.half()
-            outputs = model(input_img) 
-            f = outputs.data.cpu().float()
-            ff = ff+f
-        # norm feature
-        if opt.PCB:
-            # feature size (n,2048,6)
-            # 1. To treat every part equally, I calculate the norm for every 2048-dim part feature.
-            # 2. To keep the cosine score==1, sqrt(6) is added to norm the whole feature (2048*6).
-            fnorm = torch.norm(ff, p=2, dim=1, keepdim=True) * np.sqrt(6) 
-            ff = ff.div(fnorm.expand_as(ff))
-            ff = ff.view(ff.size(0), -1)
-        else:
-            fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
-            ff = ff.div(fnorm.expand_as(ff))
-
-        features = torch.cat((features,ff), 0)
-    return features
-
-def get_id(img_path):
-    camera_id = []
-    labels = []
-    for path, v in img_path:
-        #filename = path.split('/')[-1]
-        filename = os.path.basename(path)
-        label = filename[0:4]
-        camera = filename.split('c')[1]
-        if label[0:2]=='-1':
-            labels.append(-1)
-        else:
-            labels.append(int(label))
-        camera_id.append(int(camera[0]))
-    return camera_id, labels
-
-gallery_path = image_datasets['gallery'].imgs
-query_path = image_datasets['query'].imgs
-
-gallery_cam,gallery_label = get_id(gallery_path)
-query_cam,query_label = get_id(query_path)
-
-if opt.multi:
-    mquery_path = image_datasets['multi-query'].imgs
-    mquery_cam,mquery_label = get_id(mquery_path)
-
-######################################################################
-# Load Collected data Trained model
-print('-------test-----------')
-if opt.use_dense:
-    model_structure = ft_net_dense(opt.nclasses)
-else:
-    model_structure = ft_net(opt.nclasses)
-
-if opt.PCB:
-    model_structure = PCB(opt.nclasses)
-
-#if opt.fp16:
-#    model_structure = network_to_half(model_structure)
-
-model = load_network(model_structure)
-
-# Remove the final fc layer and classifier layer
-if opt.PCB:
-    #if opt.fp16:
-    #    model = PCB_test(model[1])
-    #else:
-        model = PCB_test(model)
-else:
-    #if opt.fp16:
-        #model[1].model.fc = nn.Sequential()
-        #model[1].classifier = nn.Sequential()
-    #else:
-        model.model.fc = nn.Sequential()
-        model.classifier.classifier = nn.Sequential()
-
-# Change to test mode
-model = model.eval()
-if use_gpu:
-    model = model.cuda()
-
-# Extract feature
-with torch.no_grad():
-    gallery_feature = extract_feature(model,dataloaders['gallery'])
-    query_feature = extract_feature(model,dataloaders['query'])
-    if opt.multi:
-        mquery_feature = extract_feature(model,dataloaders['multi-query'])
+def main(ids, name, which_epoch,use_dense, backbond=True, arc=False):
+    parser = argparse.ArgumentParser(description='Training')
+    parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
+    parser.add_argument('--which_epoch',default='last', type=str, help='0,1,2,3...or last')
+    parser.add_argument('--test_dir',default='/home/dataset/Market-1501/pytorch', type=str, help='./test_data')
+    parser.add_argument('--name', default='ft_ResNet50', type=str, help='save model path')
+    parser.add_argument('--batchsize', default=256, type=int, help='batchsize')
+    parser.add_argument('--use_dense', action='store_true', help='use densenet121' )
+    parser.add_argument('--PCB', action='store_true', help='use PCB' )
+    parser.add_argument('--multi', action='store_true', help='use multiple query' )
+    parser.add_argument('--fp16', action='store_true', help='use fp16.' )
     
-# Save to Matlab for check
-result = {'gallery_f':gallery_feature.numpy(),'gallery_label':gallery_label,'gallery_cam':gallery_cam,'query_f':query_feature.numpy(),'query_label':query_label,'query_cam':query_cam}
-scipy.io.savemat('pytorch_result.mat',result)
-if opt.multi:
-    result = {'mquery_f':mquery_feature.numpy(),'mquery_label':mquery_label,'mquery_cam':mquery_cam}
-    scipy.io.savemat('multi_query.mat',result)
+    opt = parser.parse_args()
+    ###load config###
+    # load the training config
+
+    opt.gpu_ids = ids
+    opt.name = name
+    config_path = os.path.join('./model',opt.name,'opts.yaml')
+    with open(config_path, 'r') as stream:
+            config = yaml.load(stream)
+    opt.fp16 = config['fp16'] 
+    opt.PCB = config['PCB']
+    opt.use_dense = config['use_dense']
+    
+    str_ids = opt.gpu_ids.split(',')
+    # which_epoch = opt.which_epoch
+    opt.which_epoch = which_epoch
+    name = opt.name
+    test_dir = opt.test_dir
+    
+    gpu_ids = []
+    for str_id in str_ids:
+        id = int(str_id)
+        if id >=0:
+            gpu_ids.append(id)
+    
+    # set gpu ids
+    if len(gpu_ids)>0:
+        torch.cuda.set_device(gpu_ids[0])
+        cudnn.benchmark = True
+    
+    ######################################################################
+    # Load Data
+    # ---------
+    #
+    # We will use torchvision and torch.utils.data packages for loading the
+    # data.
+    #
+    data_transforms = transforms.Compose([
+            transforms.Resize((256,128), interpolation=3),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ############### Ten Crop        
+            #transforms.TenCrop(224),
+            #transforms.Lambda(lambda crops: torch.stack(
+             #   [transforms.ToTensor()(crop) 
+              #      for crop in crops]
+               # )),
+            #transforms.Lambda(lambda crops: torch.stack(
+             #   [transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(crop)
+              #       for crop in crops]
+              # ))
+    ])
+    
+    if opt.PCB:
+        data_transforms = transforms.Compose([
+            transforms.Resize((384,192), interpolation=3),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
+        ])
+    
+    
+    data_dir = test_dir
+    
+    if opt.multi:
+        image_datasets = {x: datasets.ImageFolder( os.path.join(data_dir,x) ,data_transforms) for x in ['gallery','query','multi-query']}
+        dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
+                                                 shuffle=False, num_workers=16) for x in ['gallery','query','multi-query']}
+    else:
+        image_datasets = {x: datasets.ImageFolder( os.path.join(data_dir,x) ,data_transforms) for x in ['gallery','query']}
+        dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
+                                                 shuffle=False, num_workers=16) for x in ['gallery','query']}
+    class_names = image_datasets['query'].classes
+    use_gpu = torch.cuda.is_available()
+    
+    ######################################################################
+    # Load model
+    #---------------------------
+    def load_network(network):
+        save_path = os.path.join('./model',name,'net_%s.pth'%opt.which_epoch)
+        network.load_state_dict(torch.load(save_path))
+        return network
+    
+    
+    ######################################################################
+    # Extract feature
+    # ----------------------
+    #
+    # Extract feature from  a trained model.
+    #
+    def fliplr(img):
+        '''flip horizontal'''
+        inv_idx = torch.arange(img.size(3)-1,-1,-1).long()  # N x C x H x W
+        img_flip = img.index_select(3,inv_idx)
+        return img_flip
+    
+    def extract_feature(model,dataloaders):
+        features = torch.FloatTensor()
+        count = 0
+        for data in dataloaders:
+            img, label = data
+            n, c, h, w = img.size()
+            count += n
+            print(count)
+            if opt.use_dense:
+                ff = torch.FloatTensor(n,1024).zero_()
+            elif backbond:
+                ff = torch.FloatTensor(n,2048).zero_()
+            else:
+                # ff = torch.FloatTensor(n,2048).zero_()
+                ##########################################
+                # change bonenet or embedding
+                
+                ff = torch.FloatTensor(n, 512).zero_()
+
+            if opt.PCB:
+                ff = torch.FloatTensor(n,2048,6).zero_() # we have six parts
+            for i in range(2):
+                if(i==1):
+                    img = fliplr(img)
+                input_img = Variable(img.cuda())
+                if opt.fp16:
+                    input_img = input_img.half()
+                outputs = model(input_img) 
+                f = outputs.data.cpu().float()
+                ff = ff+f
+            # norm feature
+            if opt.PCB:
+                # feature size (n,2048,6)
+                # 1. To treat every part equally, I calculate the norm for every 2048-dim part feature.
+                # 2. To keep the cosine score==1, sqrt(6) is added to norm the whole feature (2048*6).
+                fnorm = torch.norm(ff, p=2, dim=1, keepdim=True) * np.sqrt(6) 
+                ff = ff.div(fnorm.expand_as(ff))
+                ff = ff.view(ff.size(0), -1)
+            else:
+                fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
+                ff = ff.div(fnorm.expand_as(ff))
+    
+            features = torch.cat((features,ff), 0)
+        return features
+    
+    def get_id(img_path):
+        camera_id = []
+        labels = []
+        for path, v in img_path:
+            #filename = path.split('/')[-1]
+            filename = os.path.basename(path)
+            label = filename[0:4]
+            camera = filename.split('c')[1]
+            if label[0:2]=='-1':
+                labels.append(-1)
+            else:
+                labels.append(int(label))
+            camera_id.append(int(camera[0]))
+        return camera_id, labels
+    
+    gallery_path = image_datasets['gallery'].imgs
+    query_path = image_datasets['query'].imgs
+    # print('gallery_path: {}'.format(gallery_path))
+    # print('query_path : {}'.format(query_path))
+    
+    gallery_cam,gallery_label = get_id(gallery_path)
+    query_cam,query_label = get_id(query_path)
+    
+    if opt.multi:
+        mquery_path = image_datasets['multi-query'].imgs
+        mquery_cam,mquery_label = get_id(mquery_path)
+    
+    ######################################################################
+    # Load Collected data Trained model
+    print('-------test-----------')
+    if opt.use_dense:
+        # model_structure = ft_net_dense(751)
+        #####################################3
+        # dense arc
+        model_structure = dense_arc(512)
+    elif arc:
+        
+        ###################################
+        # change load model
+        model_structure = resnet_arc(512)
+    else:
+        model_structure = ft_net(751)
+    
+    if opt.PCB:
+        model_structure = PCB(751)
+    
+    if opt.fp16:
+        model_structure = network_to_half(model_structure)
+    
+    model = load_network(model_structure)
+    
+    # Remove the final fc layer and classifier layer
+    if opt.PCB:
+        model = PCB_test(model)
+        model = network_to_half(model)
+    elif opt.fp16:
+        model[1].model.fc = nn.Sequential()
+        model[1].classifier = nn.Sequential()
+    elif backbond:
+        model.model.fc = nn.Sequential()
+        model.addblock = nn.Sequential()
+        model.classifier = nn.Sequential()
+    elif arc:
+        model.addblock = nn.Sequential()
+        model.model.fc = nn.Sequential()
+        model.classifier = nn.Sequential()
+        # 
+        #############################################\
+        #whether extract feature
+    else:
+        # model.model.fc = nn.Sequential()
+        model.classifier = nn.Sequential()
+        
+    
+    # Change to test mode
+    model = model.eval()
+    if use_gpu:
+        model = model.cuda()
+    
+    # Extract feature
+    with torch.no_grad():
+        gallery_feature = extract_feature(model,dataloaders['gallery'])
+        query_feature = extract_feature(model,dataloaders['query'])
+        if opt.multi:
+            mquery_feature = extract_feature(model,dataloaders['multi-query'])
+        
+    # Save to Matlab for check
+    result = {'gallery_f':gallery_feature.numpy(),'gallery_label':gallery_label,'gallery_cam':gallery_cam,'query_f':query_feature.numpy(),'query_label':query_label,'query_cam':query_cam}
+    scipy.io.savemat('./model/'+name+'/pytorch_result.mat',result)
+    if opt.multi:
+        result = {'mquery_f':mquery_feature.numpy(),'mquery_label':mquery_label,'mquery_cam':mquery_cam}
+        scipy.io.savemat('./model/'+name+'/multi_query.mat',result)
+
+if __name__ == '__main__':
+    main()
